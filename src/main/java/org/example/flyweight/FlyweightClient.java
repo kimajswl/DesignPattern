3604321aef5e8082f4c709ab068ec443c0274a86
Kimajswl <ajswl4763@naver.com>
2024-03-04 17:37:19 +0900

    flyweight 패턴 구현 수정

--- src/main/java/org/example/flyweight/FlyweightClient.java
+++ src/main/java/org/example/flyweight/FlyweightClient.java
@@ -4,9 +4,9 @@ public class FlyweightClient {
     public static void main(String[] args) {
         CharacterFactory characterFactory = new CharacterFactory();
 
-        System.out.println(characterFactory.createCharacter("밍밍", 100));
-        System.out.println(characterFactory.createCharacter("밍밍", 150));
-        System.out.println(characterFactory.createCharacter("빅보", 500));
-        System.out.println(characterFactory.createCharacter("카이", 300));
+        System.out.println(characterFactory.createCharacter("밍밍", 100).hashCode());
+        System.out.println(characterFactory.createCharacter("밍밍", 150).hashCode());
+        System.out.println(characterFactory.createCharacter("빅보", 500).hashCode());
+        System.out.println(characterFactory.createCharacter("카이", 300).hashCode());
     }
 }
